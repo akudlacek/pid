2ec58160519bb28342f7f1b4864d08aa159817ec
akudlacek <7029639+akudlacek@users.noreply.github.com>
2018-11-30 16:31:19 -0600

    PID: added get functions for parameters. Moved i_component clear into pid task     i_component was getting modified by the clamp_and_windup function when ki=0 PID: added time dependant function to PID PID: added saturation tracking mode and float error check     removed clamp_and_windup which was cauing erratic behavior under certain conditions PID: single definitin of pid_conf_t PID: removed direction added dedicated gain set functions PID: added component limits
    Signed-off-by: akudlacek <7029639+akudlacek@users.noreply.github.com>

--- pid.c
+++ pid.c
@@ -10,11 +10,18 @@
 
 #include "pid.h"
 
+#include <math.h>
+
+
+/**************************************************************************************************
+*                                             DEFINES
+*************************************************^************************************************/
+#define PID_LIM(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt))) //keeps within high and low range
+
 
 /**************************************************************************************************
 *                                         LOCAL PROTOTYPES
 *************************************************^************************************************/
-static inline void clamp_and_windup(float min, float max, float *i_component, float *output);
 
 
 /**************************************************************************************************
@@ -30,14 +37,23 @@ void pid_get_config_defaults(pid_conf_t *pid_conf)
 	pid_conf->kp = 0;
 	pid_conf->ki = 0;
 	pid_conf->kd = 0;
+	pid_conf->kt = 0;
 	
-	pid_conf->direction = 0;
-	pid_conf->out_min = 0;
-	pid_conf->out_max = 0;
+	pid_conf->p_max = 0;
+	pid_conf->p_min = 0;
+	pid_conf->i_max = 0;
+	pid_conf->i_min = 0;
+	pid_conf->d_max = 0;
+	pid_conf->d_min = 0;
 	
-	pid_conf->input = 0;
-	pid_conf->output = 0;
+	pid_conf->out_min   = 0;
+	pid_conf->out_max   = 0;
+	pid_conf->pid_mode  = MANUAL;
+	
+	pid_conf->input    = 0;
+	pid_conf->output   = 0;
 	pid_conf->setpoint = 0;
+	pid_conf->tick_ms  = 0;
 }
 
 /******************************************************************************
@@ -49,36 +65,49 @@ pid_return_t pid_init(pid_inst_t *pid, pid_conf_t pid_conf)
 {
 	pid_return_t pid_return;
 	
+	/*Clear PID instance*/
+	pid->last_input      = 0;
+	pid->p_component     = 0;
+	pid->i_component     = 0;
+	pid->d_component     = 0;
+	pid->error           = 0;
+	pid->last_tick_ms    = 0;
+	pid->last_output     = 0;
+	pid->last_output_sat = 0;
+	
 	/*NULL pointer check*/
-	if(pid_conf.output == 0 || pid_conf.input == 0 || pid_conf.setpoint == 0)
+	if(pid_conf.input == 0 || pid_conf.output == 0 || pid_conf.setpoint == 0 || pid_conf.tick_ms == 0)
 	{
-		pid_return = NULL_POINTER;
+		return NULL_POINTER;
 	}
 	
-	else
-	{
-		/*Set data pointers*/
-		pid->output = pid_conf.output;
-		pid->input = pid_conf.input;
-		pid->setpoint = pid_conf.setpoint;
+	/*Set data pointers*/
+	pid->conf.input    = pid_conf.input;
+	pid->conf.output   = pid_conf.output;
+	pid->conf.setpoint = pid_conf.setpoint;
+	pid->conf.tick_ms  = pid_conf.tick_ms;
 		
-		/*Set direction*/
-		pid->direction = pid_conf.direction;
+	/*Set gains*/
+	pid_set_kp(pid, pid_conf.kp);
+	pid_set_ki(pid, pid_conf.ki);
+	pid_set_kd(pid, pid_conf.kd);
+	pid_set_kt(pid, pid_conf.kt);
+	
+	/*Set PID limits*/
+	pid->conf.p_max = pid_conf.p_max;
+	pid->conf.p_min = pid_conf.p_min;
+	pid->conf.i_max = pid_conf.i_max;
+	pid->conf.i_min = pid_conf.i_min;
+	pid->conf.d_max = pid_conf.d_max;
+	pid->conf.d_min = pid_conf.d_min;
 		
-		/*Set gain*/
-		pid_return = pid_set_tuning(pid, pid_conf.kp, pid_conf.ki, pid_conf.kd);
-		
-		if(pid_return == SUCCESS)
-		{
-			/*Set output limits*/
-			pid_return = pid_set_output_limits(pid, pid_conf.out_min, pid_conf.out_max);
+	/*Set output limits*/
+	pid_return = pid_set_output_limits(pid, pid_conf.out_min, pid_conf.out_max);
 			
-			if(pid_return == SUCCESS)
-			{
-				/*Turn on PID*/
-				pid->pid_mode = AUTOMATIC;
-			}
-		}
+	if(pid_return == SUCCESS)
+	{
+		/*Turn on PID*/
+		pid_set_mode(pid, AUTOMATIC);
 	}
 	
 	return pid_return;
@@ -93,84 +122,112 @@ pid_return_t pid_init(pid_inst_t *pid, pid_conf_t pid_conf)
 pid_return_t pid_task(pid_inst_t *pid)
 {
 	pid_return_t pid_return = MANUAL_MODE;
-	float input        = 0;
-	float error        = 0;
-	float d_input      = 0;
-	float output       = 0;
+	float input             = 0;
+	float d_input           = 0;
+	float output            = 0;
+	uint32_t tick_ms        = *pid->conf.tick_ms;
+	float dt                = (float)(tick_ms - pid->last_tick_ms);
+	float saturation_error  = 0;
+	
+	/*Check for nonzero dt*/
+	if(dt <= 0.0)
+	{
+		pid_return = DT_ZERO;
+	}
 	
 	/*MANUAL mode returns without doing anything*/
-	if(pid->pid_mode == AUTOMATIC)
+	else if(pid->conf.pid_mode == AUTOMATIC)
 	{
 		/*Compute all the working error variables*/
-		input        = *pid->input;
-		error        = *pid->setpoint - input;
-		d_input = input - pid->last_input;
+		input             = *pid->conf.input;
+		pid->error        = *pid->conf.setpoint - input;
+		d_input           = input - pid->last_input;
 			
 		/****Calculate P component****/
-		pid->p_component = pid->kp * error;
+		pid->p_component = pid->conf.kp * pid->error;
 			
 		/****Calculate I component****/
-		pid->i_component += pid->ki * error;
+		/*Remove residual sum from integrator for ki of zero*/
+		if(pid->conf.ki <= 0.0)
+		{
+			pid->i_component = 0.0;
+		}
+		else
+		{
+			saturation_error = pid->last_output_sat - pid->last_output;
+			pid->i_component += ((pid->conf.ki * pid->error) + (pid->conf.kt * saturation_error)) * dt;
+		}
 			
 		/****Calculate D component****/
-		pid->d_component = pid->kd * d_input;
-			
+		pid->d_component = pid->conf.kd * (d_input / dt);
+		
+		/*Component Limits*/
+		pid->p_component = PID_LIM(pid->p_component, pid->conf.p_min, pid->conf.p_max);
+		pid->i_component = PID_LIM(pid->i_component, pid->conf.i_min, pid->conf.i_max);
+		pid->d_component = PID_LIM(pid->d_component, pid->conf.d_min, pid->conf.d_max);
+		
 		/*Sum PID components*/
 		output = pid->p_component + pid->i_component - pid->d_component;
-			
-		/*Clamp output to limits and windup protection*/
-		clamp_and_windup(pid->out_min, pid->out_max, &pid->i_component, &output);
-			
-		/*Set output*/
-		*pid->output = output;
+		
+		/*Set output with limit*/
+		*pid->conf.output = PID_LIM(output, pid->conf.out_min, pid->conf.out_max);
 			
 		/*Remember some variables for next time*/
-		pid->last_input = input;
+		pid->last_input      = input;
+		pid->last_tick_ms    = tick_ms;
+		pid->last_output     = output;
+		pid->last_output_sat = *pid->conf.output;
 
 		pid_return = SUCCESS;
+		
+		/*Check for float errors*/
+		if(!isnormal(*pid->conf.output) && *pid->conf.output != 0.0)
+		{
+			pid_return = OUTPUT_ERR;
+		}
 	}
 	
 	return pid_return;
 }
 
 /******************************************************************************
-*  \brief Set Tuning
+*  \brief Set kp
 *
 *  \note
 ******************************************************************************/
-pid_return_t pid_set_tuning(pid_inst_t *pid, float kp, float ki, float kd)
+void pid_set_kp(pid_inst_t *pid, float kp)
 {
-	pid_return_t pid_return = SUCCESS;
-	
-	/*Keep only positive PID parameters*/
-	if(kp < 0 || ki < 0 || kd < 0)
-	{
-		pid_return =  NEGATIVE_PARAM;
-	}
-	
-	else
-	{
-		/*Remove residual sum from integrator on ki of zero*/
-		if(ki <= 0.0)
-		{
-			pid->i_component = 0.0;
-		}
-		
-		/*Set new PID gain values*/
-		pid->kp = kp;
-		pid->ki = ki;
-		pid->kd = kd;
-		
-		/*Set PID direction*/
-		if(pid->direction == REVERSE)
-		{
-			pid->kp = -pid->kp;
-			pid->ki = -pid->ki;
-			pid->kd = -pid->kd;
-		}
-	}
-	
-	return pid_return;
+	pid->conf.kp = kp;
+}
+
+/******************************************************************************
+*  \brief Set ki
+*
+*  \note
+******************************************************************************/
+void pid_set_ki(pid_inst_t *pid, float ki)
+{
+	pid->conf.ki = ki;
+}
+
+/******************************************************************************
+*  \brief Set kd
+*
+*  \note
+******************************************************************************/
+void pid_set_kd(pid_inst_t *pid, float kd)
+{
+	pid->conf.kd = kd;
+}
+
+/******************************************************************************
+*  \brief Set kt
+*
+*  \note
+******************************************************************************/
+void pid_set_kt(pid_inst_t *pid, float kt)
+{
+	pid->conf.kt = kt;
 }
 
 /******************************************************************************
@@ -180,22 +237,20 @@ pid_return_t pid_set_tuning(pid_inst_t *pid, float kp, float ki, float kd)
 ******************************************************************************/
 pid_return_t pid_set_output_limits(pid_inst_t *pid, float min, float max)
 {
-	pid_return_t pid_return = SUCCESS;
-	
 	if(min >= max)
 	{
-		pid_return = MIN_GREATER_EQUAL_MAX;
+		return MIN_GREATER_EQUAL_MAX;
 	}
 	
 	else
 	{
-		pid->out_min = min;
-		pid->out_max = max;
+		pid->conf.out_min = min;
+		pid->conf.out_max = max;
 		
-		clamp_and_windup(min, max, &pid->i_component, pid->output);
+		*pid->conf.output = PID_LIM(*pid->conf.output, pid->conf.out_min, pid->conf.out_max);
 	}
 	
-	return pid_return;
+	return SUCCESS;
 }
 
 /******************************************************************************
@@ -206,53 +261,128 @@ pid_return_t pid_set_output_limits(pid_inst_t *pid, float min, float max)
 void pid_set_mode(pid_inst_t *pid, pid_mode_t pid_mode)
 {
 	/*If going from MANUAL to AUTOMATIC*/
-	if((pid->pid_mode == MANUAL) && (pid_mode == AUTOMATIC))
+	if((pid->conf.pid_mode == MANUAL) && (pid_mode == AUTOMATIC))
 	{
-		pid->i_component = *pid->output;
-		pid->last_input = *pid->input;
-		
-		clamp_and_windup(pid->out_min, pid->out_max, &pid->i_component, pid->output);
+		//pid->i_component  = *pid->output;
+		pid->last_input   = *pid->conf.input;
+		pid->last_tick_ms = *pid->conf.tick_ms;
 	}
 	
-	pid->pid_mode = pid_mode;
+	pid->conf.pid_mode = pid_mode;
 }
 
 /******************************************************************************
-*  \brief Set PID direction
+*  \brief Get kp
 *
 *  \note
 ******************************************************************************/
-void pid_set_direction(pid_inst_t *pid, pid_direction_t direction)
+float pid_get_kp(pid_inst_t pid)
 {
-	if(pid->direction != direction)
-	{
-		pid->kp = -pid->kp;
-		pid->ki = -pid->ki;
-		pid->kd = -pid->kd;
-		
-		pid->direction = direction;
-	}
+	return pid.conf.kp;
+}
+
+/******************************************************************************
+*  \brief Get ki
+*
+*  \note
+******************************************************************************/
+float pid_get_ki(pid_inst_t pid)
+{
+	return pid.conf.ki;
+}
+
+/******************************************************************************
+*  \brief Get kd
+*
+*  \note
+******************************************************************************/
+float pid_get_kd(pid_inst_t pid)
+{
+	return pid.conf.kd;
+}
+
+/******************************************************************************
+*  \brief Get kt
+*
+*  \note
+******************************************************************************/
+float pid_get_kt(pid_inst_t pid)
+{
+	return pid.conf.kt;
+}
+
+/******************************************************************************
+*  \brief Get out min
+*
+*  \note
+******************************************************************************/
+float pid_get_out_min(pid_inst_t pid)
+{
+	return pid.conf.out_min;
+}
+
+/******************************************************************************
+*  \brief Get out max
+*
+*  \note
+******************************************************************************/
+float pid_get_out_max(pid_inst_t pid)
+{
+	return pid.conf.out_max;
+}
+
+/******************************************************************************
+*  \brief Get mode
+*
+*  \note
+******************************************************************************/
+pid_mode_t pid_get_mode(pid_inst_t pid)
+{
+	return pid.conf.pid_mode;
+}
+
+/******************************************************************************
+*  \brief Get p component
+*
+*  \note
+******************************************************************************/
+float pid_get_p_component(pid_inst_t pid)
+{
+	return pid.p_component;
+}
+
+/******************************************************************************
+*  \brief Get i component
+*
+*  \note
+******************************************************************************/
+float pid_get_i_component(pid_inst_t pid)
+{
+	return pid.i_component;
+}
+
+/******************************************************************************
+*  \brief Get d component
+*
+*  \note
+******************************************************************************/
+float pid_get_d_component(pid_inst_t pid)
+{
+	return pid.d_component;
+}
+
+/******************************************************************************
+*  \brief Get error
+*
+*  \note
+******************************************************************************/
+float pid_get_error(pid_inst_t pid)
+{
+	return pid.error;
 }
 
 
 /**************************************************************************************************
 *                                         LOCAL FUNCTIONS
 *************************************************^************************************************/
-/******************************************************************************
-*  \brief Clamp and windup protection
-*
-*  \note
-******************************************************************************/
-static inline void clamp_and_windup(float min, float max, float *i_component, float *output)
-{
-	if(*output > max)
-	{
-		*i_component -= *output - max; //Integral windup protection
-		*output = max;                 //Clamp output
-	}
-	else if(*output < min)
-	{
-		*i_component += min - *output; //Integral windup protection
-		*output = min;                 //Clamp output
-	}
-}
+
--- pid.h
+++ pid.h
@@ -22,7 +22,8 @@ typedef enum
 	SUCCESS,
 	MIN_GREATER_EQUAL_MAX,
 	NULL_POINTER,
-	NEGATIVE_PARAM
+	DT_ZERO,
+	OUTPUT_ERR              //floating point error on output
 } pid_return_t;
 
 typedef enum
@@ -31,54 +32,48 @@ typedef enum
 	AUTOMATIC
 } pid_mode_t;
 
-typedef enum
-{
-	DIRECT,
-	REVERSE
-} pid_direction_t;
-
 typedef struct
 {
 	//PID gain values
-	float kp;
-	float ki;
-	float kd;
+	float kp;                     //Proportional gain
+	float ki;                     //Integral gain
+	float kd;                     //Derivative gain, derivative on measurement not filtered
+	float kt;                     //Saturation tracking gain: suggested kt = sqrt(1/ki*1/kd)
 	
-	//Configuration
-	pid_direction_t direction;
+	//PID component limits
+	float p_max;
+	float p_min;
+	float i_max;
+	float i_min;
+	float d_max;
+	float d_min;
 	float out_min;
 	float out_max;
+	
 	pid_mode_t pid_mode;
 	
 	//Data pointers
 	float *input;
 	float *output;
 	float *setpoint;
+	uint32_t *tick_ms;
+} pid_conf_t;
+
+typedef struct
+{
+	//PID configuration
+	pid_conf_t conf;
 	
 	//Dynamic data
 	float last_input;
 	float p_component;
 	float i_component;
 	float d_component;
+	float error;
+	uint32_t last_tick_ms;
+	float last_output;
+	float last_output_sat;
 } pid_inst_t;
-
-typedef struct
-{
-	//PID parameters
-	float kp;
-	float ki;
-	float kd;
-	
-	//Configuration
-	pid_direction_t direction;
-	float out_min;
-	float out_max;
-	
-	//Data pointers
-	float *input;
-	float *output;
-	float *setpoint;
-} pid_conf_t;
 
 
 /**************************************************************************************************
@@ -87,10 +82,25 @@ typedef struct
 void pid_get_config_defaults(pid_conf_t *pid_conf);
 pid_return_t pid_init(pid_inst_t *pid, pid_conf_t pid_conf);
 pid_return_t pid_task(pid_inst_t *pid);
-pid_return_t pid_set_tuning(pid_inst_t *pid, float kp, float ki, float kd);
+
+void pid_set_kp(pid_inst_t *pid, float kp);
+void pid_set_ki(pid_inst_t *pid, float ki);
+void pid_set_kd(pid_inst_t *pid, float kd);
+void pid_set_kt(pid_inst_t *pid, float kt);
 pid_return_t pid_set_output_limits(pid_inst_t *pid, float min, float max);
 void pid_set_mode(pid_inst_t *pid, pid_mode_t pid_mode);
-void pid_set_direction(pid_inst_t *pid, pid_direction_t direction);
+
+float pid_get_kp(pid_inst_t pid);
+float pid_get_ki(pid_inst_t pid);
+float pid_get_kd(pid_inst_t pid);
+float pid_get_kt(pid_inst_t pid);
+float pid_get_out_min(pid_inst_t pid);
+float pid_get_out_max(pid_inst_t pid);
+pid_mode_t pid_get_mode(pid_inst_t pid);
+float pid_get_p_component(pid_inst_t pid);
+float pid_get_i_component(pid_inst_t pid);
+float pid_get_d_component(pid_inst_t pid);
+float pid_get_error(pid_inst_t pid);
 
 
 #endif /* PID_H_ */
